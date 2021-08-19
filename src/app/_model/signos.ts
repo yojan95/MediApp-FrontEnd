2e352c4ce5cb7297b592889a0f4c3a022589ae8c
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 <estanfor95@gmail.com>
2021-08-19 18:20:44 -0500

    se añade model signos

---
+++ src/app/_model/signos.ts
@@ -0,0 +1,10 @@
+import { Paciente } from "./paciente";
+
+export class Signos{
+    idSignos?: number;
+    temperatura?: string;
+    pulso?: string;
+    ritmo?: string;
+    fecha?: string;
+    paciente: Paciente[];
+}
