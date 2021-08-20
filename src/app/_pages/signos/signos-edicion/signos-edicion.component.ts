5463d8cbf35d7f7ce3d76e4c74f619dd84c73f32
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 <estanfor95@gmail.com>
2021-08-20 18:46:17 -0500

    se añade funcionalidad para registrar y editar,eliminar,redireccionamiento

--- src/app/_pages/signos/signos-edicion/signos-edicion.component.ts
+++ src/app/_pages/signos/signos-edicion/signos-edicion.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { Paciente } from 'src/app/_model/paciente';
+import { Signos } from 'src/app/_model/signos';
+import { SignosService } from 'src/app/_service/signos.service';
 
 @Component({
   selector: 'app-signos-edicion',
@@ -7,9 +13,84 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignosEdicionComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup = new FormGroup({ });
+  id: number = 0;
+  edicion: boolean= false;
+  title:string;
+  signos = new Signos();
+
+  constructor(
+    private route: ActivatedRoute,
+    private signosService: SignosService,
+    private router:Router
+  ) { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      'id': new FormControl(0),
+      'temperatura': new FormControl(''),
+      'pulso': new FormControl(''),
+      'ritmo': new FormControl(''),
+      'fecha': new FormControl(''),
+      'idPaciente': new FormControl('')
+    });
+
+    this.route.params.subscribe((data: Params ) =>{
+      this.id = data['id'];
+      this.edicion = data['id'] != null;
+      console.log("identificacion "+this.id);
+      if(this.edicion){
+        this.title="Modificar Signos";
+      }else{
+        this.title="Registrar Signos"
+      }
+      this.initForm();
+    });
+  }
+
+  initForm(){
+    if(this.edicion){
+      this.signosService.listarPorId(this.id).subscribe(data =>{
+      
+        console.log(data);
+        this.form = new FormGroup({
+          'id': new FormControl(data.idSignos),
+          'temperatura': new FormControl(data.temperatura),
+          'pulso': new FormControl(data.pulso),
+          'ritmo': new FormControl(data.ritmo),
+          'fecha': new FormControl(data.fecha),
+          'idPaciente': new FormControl(data.paciente.nombres)
+        });
+      })
+    }
+  }
+
+  operar(){
+    let signos = new Signos();
+    signos.idSignos = this.form.value['id'];
+    signos.temperatura = this.form.value['temperatura'];
+    signos.pulso = this.form.value['pulso'];
+    signos.ritmo = this.form.value['ritmo'];
+    signos.fecha = this.form.value['fecha'];
+    signos.paciente = this.form.value['idPaciente'];
+
+    if(this.edicion){
+      this.signosService.modificar(signos).subscribe(()=>{
+        this.signosService.listar().subscribe(data => {
+          this.signosService.setSignosCambio(data);
+          this.signosService.setMensajeCambio('Se modifico');
+        })
+      });
+    }else{
+      this.signosService.registrar(signos).pipe(switchMap(() =>{
+        return this.signosService.listar();
+      }))
+      .subscribe(data =>{
+        this.signosService.setSignosCambio(data);
+        this.signosService.setMensajeCambio('Se registro');
+      });
+    }
+    this.router.navigate(['/pages/signos'])
   }
 
 }
