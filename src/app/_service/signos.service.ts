909cfd289b53b88a7b7bcff3857a4a9b5177d45b
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 <estanfor95@gmail.com>
2021-08-20 18:55:17 -0500

    se añade subject para el envio de valores a diferentes componentes

--- src/app/_service/signos.service.ts
+++ src/app/_service/signos.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Signos } from '../_model/signos';
 import { GenericService } from './generic.service';
@@ -9,11 +10,30 @@ import { GenericService } from './generic.service';
 })
 export class SignosService  extends GenericService<Signos>{
 
+  private pacienteCambio: Subject<Signos[]> = new Subject<Signos[]>();
+  private mensajeCambio: Subject<string> = new Subject<string>();
+
   
   constructor(protected http:HttpClient){ 
     super(
       http,
       `${environment.HOST}/signos`);
      }
+
+     getSignosCambio(){
+      return this.pacienteCambio.asObservable();
+    }
+  
+    setSignosCambio(lista:Signos[]){
+      this.pacienteCambio.next(lista);
+    }
+  
+    getMensajeCambio(){
+      return this.mensajeCambio.asObservable();
+    }
+  
+    setMensajeCambio(mensaje:string){
+      this.mensajeCambio.next(mensaje);
+    }
 
 }
