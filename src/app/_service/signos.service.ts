cdc588b13b1b8b7deaf8c2642e2a71c9e36d4c2f
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 <estanfor95@gmail.com>
2021-08-22 15:45:55 -0500

    se añade subject para funcionalidad de agregar paciente

--- src/app/_service/signos.service.ts
+++ src/app/_service/signos.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Paciente } from '../_model/paciente';
 import { Signos } from '../_model/signos';
 import { GenericService } from './generic.service';
 
@@ -12,6 +13,7 @@ export class SignosService  extends GenericService<Signos>{
 
   private pacienteCambio: Subject<Signos[]> = new Subject<Signos[]>();
   private mensajeCambio: Subject<string> = new Subject<string>();
+  private pacienteCambioModal: Subject<Paciente> = new Subject<Paciente>();
 
   
   constructor(protected http:HttpClient){ 
@@ -35,5 +37,14 @@ export class SignosService  extends GenericService<Signos>{
     setMensajeCambio(mensaje:string){
       this.mensajeCambio.next(mensaje);
     }
+
+    getPacienteCambio(){
+      return this.pacienteCambioModal.asObservable();
+    }
+  
+    setPacienteCambio(lista:Paciente){
+      this.pacienteCambioModal.next(lista);
+    }
+  
 
 }
